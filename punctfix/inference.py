da961a520290c88251f1c53aae094a3fd4730afa
Rasmus Arpe Fogh Jensen <Rasmus.arpe@gmail.com>
2022-03-24 14:50:24 +0100

    Fix chunking of long sequences cause by TokenClassificationPipeline (#4)
    fixes #3
    
    Also adds pylint and tests as github workflow.

--- punctfix/inference.py
+++ punctfix/inference.py
@@ -1,4 +1,6 @@
-from typing import Tuple, Dict
+from collections import Counter
+from dataclasses import dataclass
+from typing import Tuple, Dict, List
 
 import torch
 from transformers import TokenClassificationPipeline
@@ -7,18 +9,50 @@ from punctfix.models import get_custom_model_and_tokenizer, get_english_model_an
     get_danish_model_and_tokenizer, get_german_model_and_tokenizer
 
 
+class NoLanguageOrModelSelect(Exception):
+    """
+    Exception raised if you fail to specify either a language or custom model path.
+    """
+
+
+@dataclass
+class WordPrediction:
+    """
+    Dataclass to hold word and labels for inference.
+    """
+    word: str
+    labels: List[str]
+
+    @property
+    def label(self):
+        """
+        Label property. When called, at least one label should always be present.
+
+        :return: A single model label as a str
+        """
+        return Counter(self.labels).most_common(1)[0][0]
+
+
 class PunctFixer:
     """
     PunctFixer used to punctuate a given text.
     """
+
     def __init__(self, language: str = "da",
                  custom_model_path: str = None,
-                 device: str = "cuda"):
+                 word_overlap: int = 70,
+                 word_chunk_size: int = 100,
+                 device: str = "cpu"):
         """
         :param language: Valid options are "da", "de", "en", for Danish, German and English, respectively.
         :param custom_model_path: If you have a trained model yourself. If parsed, then language param will be ignored.
-        :param device: "cpu" or "cuda" to indicate where to run inference.
+        :param word_overlap: How many words should overlap in case text is too long. Defaults to 70.
+        :param word_chunk_size: How many words should a single pass consist of. Defaults to 100.
+        :param device: "cpu" or "cuda" to indicate where to run inference. Defaults to "cpu".
         """
+
+        self.word_overlap = word_overlap
+        self.word_chunk_size = word_chunk_size
 
         self.supported_languages = {
             "de": "German",
@@ -34,6 +68,9 @@ class PunctFixer:
             self.model, self.tokenizer = get_danish_model_and_tokenizer()
         elif language == "de":
             self.model, self.tokenizer = get_german_model_and_tokenizer()
+        else:
+            raise NoLanguageOrModelSelect("You need to specify either language or custom_model_path "
+                                          "when instantiating a PunctFixer.")
 
         self.model = self.model.eval()
         self.device = 0 if device == "cuda" and torch.cuda.is_available() else -1
@@ -51,6 +88,73 @@ class PunctFixer:
         """
         return self.supported_languages
 
+    @staticmethod
+    def init_word_prediction_list(words: List[str]) -> List[WordPrediction]:
+        """
+        Initialize a word prediction list i.e. a list containing WordPrediction object for each word.
+        :param words: List of words
+
+        :return: List of Word predictions
+        """
+        return [WordPrediction(word=word, labels=[]) for word in words]
+
+    def populate_word_prediction_with_labels(self, chunks: List[List[str]], word_prediction_list: List[WordPrediction]):
+        """
+        Performs predictions on all chunks of text, and adds labels to the relevant word predictions.
+
+        :param chunks: List of List of words
+        :param word_prediction_list: A list containing word predictions i.e. word and labels.
+        :return: Word predictions list with all label predictions for each word
+        """
+        for i, chunk_text in enumerate(chunks):
+            output = self.pipe(" ".join(chunk_text))
+            word_counter = 0
+            for entity in output:
+                label = entity["entity_group"]
+                text = entity["word"]
+                words_in_text = text.split(" ")
+
+                for word in words_in_text:
+                    current_index = i * self.word_chunk_size + word_counter - (i * self.word_overlap)
+
+                    # Sanity check
+                    assert word_prediction_list[current_index].word == word, \
+                        f"Something went wrong due to handling of a long text... " \
+                        f"Tried matching the word: {word} with {word_prediction_list[current_index].word}"
+                    word_prediction_list[current_index].labels.append(label)
+                    word_counter += 1
+
+        return word_prediction_list
+
+    def combine_word_predictions_into_final_text(self, word_prediction_list: List[WordPrediction]):
+        """
+        Combines all predictions for each word into a final string by checking label (majority vote or if equal
+        predictions, it chooses however Counter from itertools chooses top_n.
+
+        :param word_prediction_list: List of word predictions
+        :return: A final string with punctuation
+        """
+        final_text = []
+        auto_upper_next = False
+        for word_pred in word_prediction_list:
+            punctuated_text, auto_upper_next = self._combine_label_and_word(word_pred.label,
+                                                                            word_pred.word,
+                                                                            auto_upper_next)
+            final_text.append(punctuated_text)
+
+        return " ".join(final_text)
+
+    def split_words_into_chunks(self, words: List[str]) -> List[List[str]]:
+        """
+        Simple method to split a list of words into chunks of words with overlap.
+
+        :param words: List of words to split into chunks
+        :return: List of List of words consisting of the chunks
+        """
+        return [words[i:i + self.word_chunk_size]
+                for i in
+                range(0, len(words), self.word_chunk_size - self.word_overlap)]
+
     def punctuate(self, text: str) -> str:
         """
         Punctuates given text.
@@ -58,39 +162,42 @@ class PunctFixer:
         :param text: A lowercase text with no punctuation.
         :return: A punctuated text.
         """
-        output = self.pipe(text)
-        final_text = []
-        auto_upper_next = False
-        for entity in output:
-            label = entity["entity_group"]
-            text = entity["word"]
-            punctuated_text, auto_upper_next = self._combine_label_and_text(label, text, auto_upper_next)
-            final_text.append(punctuated_text)
-
-        return " ".join(final_text)
-
-    @staticmethod
-    def _combine_label_and_text(label: str, text: str, auto_uppercase: bool = False) -> Tuple[str, bool]:
         words = text.split(" ")
 
+        # If we have a long sequence of text (measured by words), we split it into chunks
+        chunks = []
+        if len(words) >= self.word_chunk_size:
+            chunks = self.split_words_into_chunks(words)
+        else:
+            chunks.append(words)
+
+        # We create a word prediction list and then combine the predictions to to final text
+        word_prediction_list = self.init_word_prediction_list(words)
+        word_prediction_list = self.populate_word_prediction_with_labels(chunks, word_prediction_list)
+        return self.combine_word_predictions_into_final_text(word_prediction_list)
+
+    @staticmethod
+    def _combine_label_and_word(label: str, word: str, auto_uppercase: bool = False) -> Tuple[str, bool]:
+        """
+        Combines label and word into a single string by looking at the label to see what should be changed.
+
+        :param label: Punctuation label from the model
+        :param word: Word to handle
+        :param auto_uppercase: Whether automatically uppercase independent of label
+        :return: Tuple with a str consisting of the word with relevant punctuation and
+        whether to auto capitalize next word
+        """
         next_auto_uppercase = False
+        if label[-1] == "U":
+            word = word.capitalize()
 
-        new_words = []
-        for word in words:
-            if label[-1] == "U":
-                punct_wrd = word.capitalize()
-            else:
-                punct_wrd = word
+        if label[0] != "O":
+            word += label[0]
 
-            if label[0] != "O":
-                punct_wrd += label[0]
-
-            new_words.append(punct_wrd)
-
-        if auto_uppercase and new_words[0]:
-            new_words[0] = new_words[0].capitalize()
+        if auto_uppercase:
+            word = word.capitalize()
 
         if label[0] != "0" and label[0] in [".", "!", "?"]:
             next_auto_uppercase = True
 
-        return " ".join(new_words), next_auto_uppercase
+        return word, next_auto_uppercase
--- tests/test_punctuation.py
+++ tests/test_punctuation.py
@@ -8,8 +8,11 @@ class DanishPunctuationRestorationTest(unittest.TestCase):
 
     def setUp(self) -> None:
         super().setUp()
-
         self.model = PunctFixer(language="da")
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        self.model = None
 
     def test_sample01(self):
         model_input = "mit navn det er rasmus og jeg kommer fra firmaet alvenir " \
@@ -36,8 +39,11 @@ class EnglishPunctuationRestorationTest(unittest.TestCase):
 
     def setUp(self) -> None:
         super().setUp()
-
         self.model = PunctFixer(language="en")
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        self.model = None
 
     def test_sample01(self):
         model_input = "hello i come from denmark and i am very good at english"
@@ -61,8 +67,11 @@ class GermanPunctuationRestorationTest(unittest.TestCase):
 
     def setUp(self) -> None:
         super().setUp()
-
         self.model = PunctFixer(language="de")
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        self.model = None
 
     def test_sample01(self):
         model_input = "oscar geht einkaufen in einen großen supermarkt seine einkaufsliste ist lang er " \
@@ -75,9 +84,51 @@ class GermanPunctuationRestorationTest(unittest.TestCase):
         self.assertEqual(actual_output, expected_output)
 
 
+class HandlingOfLargeTextsTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.model = PunctFixer(language="da")
+        self.long_text = "halløj derude og vel kom til en podcast med jacob og neviton du kan lige starte med at fortælle lidt om dig selv ja ja men det er så mig der jakob den ene halvdel af det her potkarsthold det er jo noget nyt for os det her det er mogens amatører det måske også lige onderestmidtet det var fald den første potkarstvidhverår så vi var i korte introducerer introns vase bare bare fortæller om det er jo næsten lige ja vi faldt næsten jævnaldrende vi bor i samme by ja vi bor i viborg vi bor og stort set helt års liv er det vær du det har jeg larlasesjeg ar altid sådan tilhørt vi går jeg kommer jo fra en lille landsby men jeg tror de kom til at høre mere om osten sener han jeg skal nok komme sådan lidt men gode personlige historier om nu vi jeg tog lidt mærkelig fyr som vi nu og vi har en hel del beatet er det må man sige en grund til vi egentlig startede mig på et kaste fordi at vi altid vi gør det dem på at snakke om alle mulige ting i flere timer faktisk så tog jeg mig selv lige tænk på tænk tanken om at staten podcast det er jo lidt maiestremehvertfald efterhånden her det er så en sjov det er sjovt når at se til bag på så tænkte jeg hvorfor ikke og det er faktisk hende det første ting vi kan huske vi latham jeg var så ikke en kot karst men men jeg skulle hente nyredtanengang efter en bitur og jeg havde ikke sådan jeg havde kendt ham en nu er det måske er og de ting vi snakker om det var ikke bare sådan en af de to ting det var virkelig det var lige for tegnefilmen til livet altså være mening med livet så er der er ikke noget bestemt i komme til at høre de klokken tre om natten efter sådan bytur koge rundt i en bil det er bare gode tider og penge til på den nu ja ser for i det korona nu har jeg så sammen med det lidt anvende ap vi sidst nogen der hører med her i to tusinde og niogtyve så kun leve en palmi der er så lidt til historie børn en ny anne de vi bare snart med denne uge men vi kan jo også komme op med et navn til podcasten ja det er og sådan for titemitersi jamen vi har venner kan læge for kort og godt for at sige i kor et godt ja ser han det års ugerne jamen jeg ved silende med at sige nej det var i hvert fald bare lige sådan en kort introduktion jeg har tænkt på hvad vi skal snakke om næste gang og det kan de vi lave i en ilespoyler ja til folket i gik jeg ned at for altså jakob et meget formelt når jeg trækker med amrepoasnakerom mer så så meget åben odsngetsonogeernår han snakker om at så gar krimeligterdetså men nå hvor sprængt om ja hvordan det ændrer så altså hvordan vi var kun så hvordan børn er nu han når der er jo sket ikke mere og så hvis der sket nogle sjove ting og brøndum som jeg lige kan huske sjove fortællinger jeg vil jo i stå jeg er lige sket som vil det tænker en bar og så kommer vi jo hundrede procent af indre så kom nok rundt omkring hvad der lige sker så det hvert fald det ja jeg har hvert fad ikke med at sige jamen skal vi afslutte den for i dag så skal du så sige os jeg ved ikke hvordan migarmename det bliver i hvert fald bedre bedre betidehkavisde sige vor velkendende ord gør når vi alle det er ikke så kendt fordi det er første gang men det bliver det for en på en tidspunkt færdig"
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        self.model = None
+        self.long_text = None
+
+    def test_multiple_chunk_size_and_padding_configs(self):
+        configs_to_test = [
+            (50, 5),
+            (50, 10),
+            (50, 20),
+            (50, 40),
+            (100, 5),
+            (100, 20),
+            (100, 40),
+            (100, 80),
+            (150, 10),
+            (150, 20),
+            (150, 50),
+            (150, 80),
+            (150, 120),
+            (200, 50),
+            (200, 100),
+            (200, 150),
+            (200, 180)
+        ]
+
+        for chunk_size, overlap in configs_to_test:
+            self.model.word_overlap = overlap
+            self.model.word_chunk_size = chunk_size
+
+            actual_output = self.model.punctuate(self.long_text)
+            self.assertIsNotNone(actual_output)
+
+
 class GenerelFunctionalityTest(unittest.TestCase):
 
-    def test_if_gpu_not_available_default_cpu(self):
+    def setUp(self) -> None:
+        super().setUp()
         # Setup
         self.torch_cuda_patch = patch(
             'punctfix.inference.torch.cuda.is_available'
@@ -90,11 +141,21 @@ class GenerelFunctionalityTest(unittest.TestCase):
         )
         self.token_classification_pipeline_mock: MagicMock = self.token_classification_pipeline_patch.start()
 
-        # Test
+    def test_if_gpu_not_available_default_cpu(self):
+        # When
         self.model = PunctFixer(language="da", device="cuda")
 
-        # Verify
+        # Expect
         self.token_classification_pipeline_mock.assert_called_once_with(model=ANY,
                                                                         tokenizer=ANY,
                                                                         aggregation_strategy="first",
                                                                         device=-1)
+
+    def tearDown(self) -> None:
+        super().tearDown()
+        self.torch_cuda_patch.stop()
+        self.token_classification_pipeline_patch.stop()
+
+
+if __name__ == '__main__':
+    unittest.main()
